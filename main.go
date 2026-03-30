f1312fb781c9db0b1e8beff50ba15309030742f8
onyx-and-iris <code@onyxandiris.online>
2026-03-30 18:49:09 +0100

    move versionFromBuild

--- main.go
+++ main.go
@@ -12,12 +12,6 @@ import (
 
 var version string // Version of the CLI, set during build time
 
-func main() {
-	if err := fang.Execute(context.Background(), cmd.RootCmd, fang.WithVersion(versionFromBuild())); err != nil {
-		os.Exit(1)
-	}
-}
-
 func versionFromBuild() string {
 	if version != "" {
 		return version
@@ -28,4 +22,10 @@ func versionFromBuild() string {
 		return "(unable to read version)"
 	}
 	return strings.Split(info.Main.Version, "-")[0]
+}
+
+func main() {
+	if err := fang.Execute(context.Background(), cmd.RootCmd, fang.WithVersion(versionFromBuild())); err != nil {
+		os.Exit(1)
+	}
 }
